bb996443fce5257fa739438eb2a7d25c23b1abe9
Ploshka <folga12381@gmail.com>
2024-09-29 15:42:03 +1000

    Struct hierarchy added

--- include/bmpreader/structs/Structs.hpp
+++ include/bmpreader/structs/Structs.hpp
@@ -18,89 +18,61 @@ struct BitMapHeader
 
 
     BitMapHeader(uint32_t _size);
+    virtual ~BitMapHeader() = default;
 };
 
-struct BitMapV5Header : public BitMapHeader
+struct BitMapCoreHeader : public BitMapHeader
 {
-  int32_t  bV5Width;
-  int32_t  bV5Height;
-  uint16_t bV5Planes;
-  uint16_t bV5BitCount;
-  uint32_t bV5Compression;
-  uint32_t bV5SizeImage;
-  int32_t  bV5XPelsPerMeter;
-  int32_t  bV5YPelsPerMeter;
-  uint32_t bV5ClrUsed;
-  uint32_t bV5ClrImportant;
-  uint32_t bV5RedMask;
-  uint32_t bV5GreenMask;
-  uint32_t bV5BlueMask;
-  uint32_t bV5AlphaMask;
-  uint32_t bV5CSType;
-  uint8_t  bV5Endpoints[36];
-  uint32_t bV5GammaRed;
-  uint32_t bV5GammaGreen;
-  uint32_t bV5GammaBlue;
-  uint32_t bV5Intent;
-  uint32_t bV5ProfileData;
-  uint32_t bV5ProfileSize;
-  uint32_t bV5Reserved;
+    uint16_t bcWidth;
+    uint16_t bcHeight;
+    uint16_t bcPlanes;
+    uint16_t bcBitCount;
 
-    BitMapV5Header(uint32_t _size);
+    BitMapCoreHeader(uint32_t _size);
 };
-const std::size_t bitMapV5HeaderSize = 124;
-
-struct BitMapV4Header : public BitMapHeader
-{
-  int32_t  bV4Width;
-  int32_t  bV4Height;
-  uint16_t bV4Planes;
-  uint16_t bV4BitCount;
-  uint32_t bV4V4Compression;
-  uint32_t bV4SizeImage;
-  int32_t  bV4XPelsPerMeter;
-  int32_t  bV4YPelsPerMeter;
-  uint32_t bV4ClrUsed;
-  uint32_t bV4ClrImportant;
-  uint32_t bV4RedMask;
-  uint32_t bV4GreenMask;
-  uint32_t bV4BlueMask;
-  uint32_t bV4AlphaMask;
-  uint32_t bV4CSType;
-  uint8_t  bV4Endpoints[36];
-  uint32_t bV4GammaRed;
-  uint32_t bV4GammaGreen;
-  uint32_t bV4GammaBlue;
-
-    BitMapV4Header(uint32_t _size);
-};
-const std::size_t bitMapV4HeaderSize = 108;
+const std::size_t bitMapCoreHeaderSize = 12;
 
 struct BitMapInfoHeader : public BitMapHeader
 {
-  int32_t  biWidth;
-  int32_t  biHeight;
-  uint16_t biPlanes;
-  uint16_t biBitCount;
-  uint32_t biCompression;
-  uint32_t biSizeImage;
-  int32_t  biXPelsPerMeter;
-  int32_t  biYPelsPerMeter;
-  uint32_t biClrUsed;
-  uint32_t biClrImportant;
+    int32_t  biWidth;
+    int32_t  biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    int32_t  biXPelsPerMeter;
+    int32_t  biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
 
     BitMapInfoHeader(uint32_t _size);
 };
 const std::size_t bitMapInfoHeaderSize = 40;
 
-struct BitMapCoreHeader : public BitMapHeader
+struct BitMapV4Header : public BitMapInfoHeader
 {
-  uint16_t bcWidth;
-  uint16_t bcHeight;
-  uint16_t bcPlanes;
-  uint16_t bcBitCount;
+    uint32_t bV4RedMask;
+    uint32_t bV4GreenMask;
+    uint32_t bV4BlueMask;
+    uint32_t bV4AlphaMask;
+    uint32_t bV4CSType;
+    uint8_t  bV4Endpoints[36];
+    uint32_t bV4GammaRed;
+    uint32_t bV4GammaGreen;
+    uint32_t bV4GammaBlue;
 
-    BitMapCoreHeader(uint32_t _size);
+    BitMapV4Header(uint32_t _size);
 };
-const std::size_t bitMapCoreHeaderSize = 12;
+const std::size_t bitMapV4HeaderSize = 108;
+
+struct BitMapV5Header : public BitMapV4Header
+{
+    uint32_t bV5Intent;
+    uint32_t bV5ProfileData;
+    uint32_t bV5ProfileSize;
+    uint32_t bV5Reserved;
+
+    BitMapV5Header(uint32_t _size);
+};
+const std::size_t bitMapV5HeaderSize = 124;
 
