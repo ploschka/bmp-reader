2db59eaa7c238352c04c580feba3fc5c6fd5a984
Ploshka <folga12381@gmail.com>
2024-09-29 16:06:11 +1000

    Constructor fix

--- src/Structs.cpp
+++ src/Structs.cpp
@@ -2,8 +2,8 @@
 #include <cstdint>
 
 BitMapHeader::BitMapHeader(uint32_t _size) : size(_size) {}
-BitMapV5Header::BitMapV5Header(uint32_t _size) : BitMapHeader(_size) {}
-BitMapV4Header::BitMapV4Header(uint32_t _size) : BitMapHeader(_size) {}
+BitMapV5Header::BitMapV5Header(uint32_t _size) : BitMapV4Header(_size) {}
+BitMapV4Header::BitMapV4Header(uint32_t _size) : BitMapInfoHeader(_size) {}
 BitMapInfoHeader::BitMapInfoHeader(uint32_t _size) : BitMapHeader(_size) {}
 BitMapCoreHeader::BitMapCoreHeader(uint32_t _size) : BitMapHeader(_size) {}
 
